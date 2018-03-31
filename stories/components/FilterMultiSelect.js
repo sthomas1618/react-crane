42cdd0fe11d2a822f1b4889b9466ac02bb2979ac
Vicky Holcomb <32145363+vicky-holcomb@users.noreply.github.com>
2018-03-31 13:37:48 -0700

    Allow multi-select filtering (#7)
    * Added grouping capabilities to single and multi-select drop downs.  Added select all functionality to multi-select drop downs.

---
+++ stories/components/FilterMultiSelect.js
@@ -0,0 +1,89 @@
+import _ from 'lodash'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { MultiSelect } from '../../src'
+import { filterOptions, getSelectValue } from '../../src/select/utils'
+
+export default class FilterMultiSelect extends React.Component {
+  static propTypes = {
+    allowSelectAll: PropTypes.bool,
+    allOption: PropTypes.object,
+    groupByKey: PropTypes.string,
+    groupTitleKey: PropTypes.string,
+    groupValueKey: PropTypes.string,
+    labelKey: PropTypes.string,
+    name: PropTypes.string,
+    options: PropTypes.array.isRequired,
+    onChange: PropTypes.func,
+    value: PropTypes.array,
+    valueKey: PropTypes.string
+  }
+
+  static defaultProps = {
+    allowSelectAll: false,
+    allOption: {
+      value: 'Select All',
+      id: '*'
+    },
+    groupByKey: '',
+    groupTitleKey: 'title',
+    groupValueKey: 'groupId',
+    labelKey: '',
+    name: '',
+    onChange: null,
+    value: null,
+    valueKey: 'value'
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputValue: '',
+      value: []
+    }
+  }
+
+  onChange = (event) => {
+    const { onChange } = this.props
+
+    this.setState({ value: event.value.map(val => (val[this.props.valueKey])) })
+
+    if (onChange) {
+      onChange(event)
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ inputValue: event.value })
+  }
+
+  getSelectValue = (props) => {
+    const selectValueProps = {
+      ...props,
+      options: this.props.options
+    }
+    return getSelectValue(selectValueProps)
+  }
+
+  render() {
+    const { options } = this.props
+    const { inputValue, value } = this.state
+    const controlProps = _.omit(this.props, 'onInputChange', 'options')
+    const opts = filterOptions(options, inputValue, this.props)
+
+    return (
+      <MultiSelect
+        {...controlProps}
+        inputValue={inputValue}
+        options={opts}
+        onChange={this.onChange}
+        onInputChange={this.onInputChange}
+        getSelectValue={this.getSelectValue}
+        value={value}
+        showInput
+        ignoreCase
+      />
+    )
+  }
+}
--- stories/components/index.js
+++ stories/components/index.js
@@ -3,6 +3,8 @@ export { default as BasicSearchSelect } from './BasicSearchSelect'
 export { default as BasicFilterSelect } from './BasicFilterSelect'
 export { default as BasicSelect } from './BasicSelect'
 
+export { default as FilterMultiSelect } from './FilterMultiSelect'
+
 export { default as SimpleSelectWithStringValue } from './SimpleSelectWithStringValue'
 export { default as MultiSelectWithStringValues } from './MultiSelectWithStringValues'
 

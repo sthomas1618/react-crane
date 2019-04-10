5024378b26fc4ffa00a57e488939e0b642c03157
Vicky Holcomb <vholcomb78@gmail.com>
2019-04-10 09:26:36 -0700

    Accessibility Improvements
    Added the a11y plugin to storybook
    Updated placeholder font color so the contrast is sufficient
    Added aria-label to open/close arrow
    Updated aria roles for placeholder and input
    Added aria-controls for input/filterable dropdown.  Updated renderMenu function to render an empty div if it's not open. Aria rules require the aria-controlled value to be visible (even if there's nothing in it).

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
+import '@storybook/addon-a11y/register'
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register'
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y'
+
+addDecorator(checkA11y)
 
 function loadStories() {
   require('../stories');

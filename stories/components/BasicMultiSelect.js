557646d1829b0b9774bc66a9d1067c86465c9398
Vicky Holcomb <vholcomb78@gmail.com>
2019-04-15 11:59:25 -0700

    Updated option aria-selected calculation to cover more value types and fixed grouping/select all issue in stories.

--- stories/components/BasicMultiSelect.js
+++ stories/components/BasicMultiSelect.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
+import { flattenOptions } from '../../src/select/utils'
 import { MultiSelect } from '../../src'
 
 class BasicMultiSelect extends Component {
+  static propTypes = {
+    allOption: PropTypes.object,
+    allowSelectAll: PropTypes.bool,
+    options: PropTypes.array.isRequired
+  }
+
+  static defaultProps = {
+    allOption: {
+      value: 'Select All',
+      id: '*'
+    },
+    allowSelectAll: false
+  }
+
   constructor(props) {
     super(props)
 
@@ -15,10 +30,17 @@ class BasicMultiSelect extends Component {
     this.setState({ value: event.value })
   }
 
+  isAllSelected = (values) => {
+    const { allOption, allowSelectAll, options } = this.props
+    const flatOpts = flattenOptions(options, allowSelectAll, allOption)
+    return values.length === flatOpts.length - 1
+  }
+
   render() {
     return (
       <MultiSelect
         {...this.props}
+        isAllSelected={this.isAllSelected}
         onChange={this.onChange}
         value={this.state.value}
       />

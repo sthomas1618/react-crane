249f2f1bf7f7d558feb6c6bcc9f5dfaea1e60d0b
Jaymi <jaymialexa@gmail.com>
2021-04-26 15:24:06 -0400

    remove the plural s from static options

--- src/select/utils/PropTypes.js
+++ src/select/utils/PropTypes.js
@@ -52,7 +52,7 @@ export const simpleSelectPropTypes = {
   optionGroupRenderer: PropTypes.func,
   optionRenderer: PropTypes.func,
   options: PropTypes.array,
-  staticOptions: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  staticOption: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
   showInput: PropTypes.bool,
   value: PropTypes.oneOfType([

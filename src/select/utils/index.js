7b63470f73fc94d42340332e2b2ebcbe0065b722
Jaymi <jaymialexa@gmail.com>
2021-06-07 14:14:52 -0400

    fix axe issues and stories

--- src/select/utils/index.js
+++ src/select/utils/index.js
@@ -1,6 +1,7 @@
 export { default as filterOptions } from './filterOptions'
 export { default as flattenOptions } from './flattenOptions'
 export { default as getLabel } from './getLabel'
+export { default as getOptionLabel } from './getOptionLabel'
 export { default as getSelectValue } from './getSelectValue'
 export { default as isSelected } from './isSelected'
 export { default as isValueEqual } from './isValueEqual'

851d990f193f091a6911a00d63bc334bd5f4c4b8
Stephen Thomas <sthomas1618@gmail.com>
2022-03-10 14:21:46 -0800

    FIX: Adjust a11y attributes for JAWs
    Add aria-activedescendant to Input so screen-readers like JAWs can know which option is "focused" while DOM focus remains on the input.
    Move aria-controls to Input per aria standards.
    Add aria-owns to the combobox element so screen-readers know the listbox should immediately follows the input in reading order.
    Remove tabindex=0 from Option. Unneeded.
    Give the menu element a unique id.
    
    https://www.w3.org/TR/wai-aria-practices-1.1/examples/combobox/aria1.1pattern/listbox-combo.html

--- src/select/FocusPlaceholder.js
+++ src/select/FocusPlaceholder.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 
 class FocusPlaceholder extends PureComponent {
   render() {
-    const { onBlur, onFocus, getRef } = this.props
+    const { onBlur, onFocus, getRef, id, ...ariaProps } = this.props
 
     return (
       <input
+        id={id}
+        aria-activedescendant={ariaProps['aria-activedescendant'] || undefined}
+        aria-controls={ariaProps['aria-controls'] || undefined}
         aria-label="placeholder text"
         className="crane-select-focus-placeholder"
         onBlur={onBlur}
@@ -19,9 +22,17 @@ class FocusPlaceholder extends PureComponent {
 }
 
 FocusPlaceholder.propTypes = {
+  'aria-activedescendant': PropTypes.string,
+  'aria-controls': PropTypes.string,
   getRef: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
   onBlur: PropTypes.func.isRequired,
   onFocus: PropTypes.func.isRequired
+}
+
+FocusPlaceholder.defaultProps = {
+  'aria-activedescendant': undefined,
+  'aria-controls': undefined
 }
 
 export default FocusPlaceholder
